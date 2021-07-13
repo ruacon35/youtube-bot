6234aae7e4eb0930b623a5f724e77ea3743fe169
ruacon35 <donghonguquen04@gmail.com>
2021-07-13 13:50:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-import  os
-
-BOT_TOKEN = os.environ.get("1842095566:AAEeLtTaTqvPPjgeckJ6VqDI1Clp975joM0")
-APP_ID = int(os.environ.get("6212937"))
-API_HASH = os.environ.get("eb3b1c92e8649791fa727410a897adf7")
-
-youtube_next_fetch = 0  # time in minute
-
-
-EDIT_TIME = 5
+bot_token = "1842095566:AAEeLtTaTqvPPjgeckJ6VqDI1Clp975joM0"
+app_id = "6212937"
+api_hash = "eb3b1c92e8649791fa727410a897adf7"
